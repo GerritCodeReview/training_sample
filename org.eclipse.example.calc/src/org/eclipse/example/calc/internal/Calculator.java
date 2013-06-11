51e14ddb7f92a87b013c87c07b59fab0b4663888
Alvaro Lopez Tijeras <altijeras77@gmail.com>
2013-06-11 12:50:35 +0200

    Removed multiply operation from a linux client. Upstream to CodeReview. Done.
    Change-Id: I3a30a6ccf3bb1de17dc99b35af7f7c284e1bc1ed

--- org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
+++ org.eclipse.example.calc/src/org/eclipse/example/calc/internal/Calculator.java
@@ -39,9 +39,8 @@ public class Calculator {
 		new Equals();
 		new Minus();
 		new Plus();
-		new Square();
 		new Divide();
-		new Multiply();
+		new Square();
 	}
 
 	private void calculate(String cmdName) {
